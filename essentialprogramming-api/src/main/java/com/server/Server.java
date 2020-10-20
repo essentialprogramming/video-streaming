591685bce2e35259ae1406a782fe666f02767522
petrisor.man <softpman@alarm.com>
2020-10-20 18:28:06 +0300

    added project structure 1

--- essentialprogramming-api/src/main/java/com/server/Server.java
+++ essentialprogramming-api/src/main/java/com/server/Server.java
@@ -13,7 +13,7 @@ public class Server {
 			throws ServletException {
 
 		final String  host = getProperty("undertow.host", "0.0.0.0");
-		final Integer port = getProperty("undertow.port", 8080);
+		final Integer port = getProperty("undertow.port", 8082);
 
 		final UndertowServer server = new UndertowServer(host, port, "essentialProgramming.jar");
 		
